b90c998634c28999e70209fd520647a3d0cdff9b
Kw <Kwhitlock1217@gmail.com>
2021-01-12 17:31:41 -0800

    update form element, add javascript function for validation.

--- script.js
+++ script.js
@@ -7,11 +7,32 @@ const nameError = document.querySelector('#nameError');
 const formError = document.querySelector('#error');
 const success = document.querySelector('.icon-success');
 const error = document.querySelector('.icon-error');
-const nextSibling = success.nextElementSibling;
-const prevSibling = error.previousElementSibling;
+// const nextSibling = success.nextElementSibling;
+// const prevSibling = error.previousElementSibling;
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     validation();
 });
+
+
+
+function validation() {
+    //get values from the inputs 
+   if (fullName.value <= 2 ) {     
+        //show error icon
+        error.classList.remove('d-none');
+        //hide success icon
+        success.classList.add('d-none');
+        //show error message
+        nameError.textContent = 'Name must be at least 2 characters long';
+        
+    } else {
+        //show success icon
+        success.classList.remove('d-none');
+        //hide error icon
+        error.classList.add('d-none');
+        nameError.textContent = '';
+    } 
+}
