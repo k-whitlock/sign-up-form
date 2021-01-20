a3a968554a2ba319836cf40356fefecba5dc1b6b
Kw <Kwhitlock1217@gmail.com>
2021-01-19 21:45:27 -0800

    Remove icons, add red border around inputs when fields are invalid, updated CSS and JS to reflect changes.

--- script.js
+++ script.js
@@ -2,37 +2,85 @@ const form = document.querySelector('#form');
 const fullName = document.querySelector('#name');
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
-const pwCheck = document.querySelector('#pwcheck');
+const pwCheck = document.querySelector('#pwCheck');
 const nameError = document.querySelector('#nameError');
-const formError = document.querySelector('#error');
-const success = document.querySelector('.icon-success');
-const error = document.querySelector('.icon-error');
-// const nextSibling = success.nextElementSibling;
-// const prevSibling = error.previousElementSibling;
+const pwError = document.querySelector('#pwError');
+const pwCheckError  = document.querySelector('#pwCheckError');
+
+
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     validation();
+    
 });
 
 
 
-function validation() {
-    //get values from the inputs 
-   if (fullName.value <= 2 ) {     
-        //show error icon
-        error.classList.remove('d-none');
-        //hide success icon
-        success.classList.add('d-none');
+ function validation() {
+    //name validation
+   if (fullName.value.length < 2) {   
+        //remove default border
+        fullName.classList.remove('border-input');
+        //add red border
+        fullName.classList.add('border-error');
         //show error message
         nameError.textContent = 'Name must be at least 2 characters long';
-        
+    
     } else {
-        //show success icon
-        success.classList.remove('d-none');
-        //hide error icon
-        error.classList.add('d-none');
+        //remove red border
+        fullName.classList.remove('border-error');   
+        //add default border
+        fullName.classList.add('border-input');  
+        //show empty message
         nameError.textContent = '';
+        
     } 
-}
+    //email validation
+    if (email.value.indexOf("@") == -1 || email.value.length < 6) {       
+        email.classList.remove('border-input');
+        email.classList.add('border-error');
+        emailError.textContent = 'Please enter a valid email';
+
+    } else {
+        email.classList.remove('border-error');   
+        email.classList.add('border-input');  
+        emailError.textContent = '';
+
+    }
+    //password validation
+    if (password.value.length < 8) {       
+        password.classList.remove('border-input');
+        password.classList.add('border-error');
+        pwError.textContent = 'Password must be at least 8 characters long';
+
+    
+    } else if (password.value.length >= 20) {       
+        password.classList.remove('border-input');
+        password.classList.add('border-error');
+        pwError.textContent = 'Password must be less than 20 characters long';
+    
+    } else {
+        password.classList.remove('border-error');   
+        password.classList.add('border-input');  
+        pwError.textContent = '';
+    } 
+    //password confirmation validation
+    if (pwCheck.value != password.value) {       
+        pwCheck.classList.remove('border-input');
+        pwCheck.classList.add('border-error');
+        pwCheckError.textContent = 'Password must match';
+
+    } else {
+        pwCheck.classList.remove('border-error');   
+        pwCheck.classList.add('border-input');  
+        pwCheckError.textContent = '';
+        
+        }    
+ }
+
+
+
+
+
